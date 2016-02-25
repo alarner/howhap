acdb11c3359f6463af257425f7d5ea0a51fb2a16
Aaron Larner <anlarner@gmail.com>
2016-02-25 17:41:56 -0600

    Update build process

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 let expect = require('chai').expect;
-let Howhap = require('../src/index.js');
+let Howhap = require('../dist/index.js');
 describe('Howhap', function() {
 	describe('errors', function() {
 		it('should not allow non-objects for the first parameter of the constructor', function() {
